35816bc8ce2ec11be9897c742bb1448f8a8a2c7e
ClimaCare <imetodi90@gmail.com>
2024-12-05 11:02:00 +0200

    task1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ConsoleApp6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Choose a task to execute (1-5):");
+
+        int taskNumber = int.Parse(Console.ReadLine());
+        switch (taskNumber)
+        {
+            case 1:
+                Task1Product.Run();
+                break;
+            //case 2:
+            //    Task2Demo.Run();
+            //    break;
+            //case 3:
+            //    Task3Demo.Run();
+            //    break;
+            //case 4:
+            //    Task4Demo.Run();
+            //    break;
+            //case 5:
+            //    Task5Demo.Run();
+            //    break;
+            default:
+                Console.WriteLine("Invalid option!");
+                break;
+        }
+    }
+}
---
+++ Task1.cs
@@ -0,0 +1,53 @@
+﻿namespace ConsoleApp6;
+public class Product // Task 1
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+
+    public Product()
+    {
+        this.Name = "Unknown";
+        this.Price = 0;
+        this.Quantity = 0;
+    }
+
+    public Product(string name, decimal price, int quantity)
+    {
+        this.Name = name;
+        this.Price = price;
+        this.Quantity = quantity;
+    }
+
+    public decimal CalculateTotalCost()
+    {
+        return Price * Quantity;
+    }
+}
+
+public static class Task1Product
+{
+    public static void Run()
+    {
+        Console.WriteLine("Enter product name:");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Enter product price:");
+        decimal price;
+        while (!decimal.TryParse(Console.ReadLine(), out price) || price < 0)
+        {
+            Console.WriteLine("Invalid input. Please enter a positive decimal value for the price:");
+        }
+
+        Console.WriteLine("Enter product quantity:");
+        int quantity;
+        while (!int.TryParse(Console.ReadLine(), out quantity) || quantity < 0)
+        {
+            Console.WriteLine("Invalid input. Please enter a non-negative integer for the quantity:");
+        }
+
+        var product = new Product(name, price, quantity);
+        Console.WriteLine($"Product: {product.Name}, Total Cost: {product.CalculateTotalCost()}");
+    }
+}
+
